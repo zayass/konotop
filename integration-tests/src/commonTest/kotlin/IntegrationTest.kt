58a5e68e4f796f1090bcfc4510bde2990ef0de49
Anton Pogonets <zayass@gmail.com>
2024-04-07 22:27:08 +0200

    Implement Buildee.baseUrl

--- integration-tests/src/commonTest/kotlin/IntegrationTest.kt
+++ integration-tests/src/commonTest/kotlin/IntegrationTest.kt
@@ -1,34 +1,13 @@
-import io.ktor.client.*
-import io.ktor.client.plugins.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
 import konotop.Konotop
 import konotop.integration.RecipesApi
 import kotlinx.coroutines.test.runTest
-import kotlinx.serialization.json.Json
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class IntegrationTest {
-    private val httpClient = HttpClient {
-        install(ContentNegotiation) {
-            json(Json {
-                ignoreUnknownKeys = true
-            })
-        }
-
-        defaultRequest {
-            url {
-                host = "dummyjson.com"
-                protocol = URLProtocol.HTTPS
-            }
-        }
-    }
-
     private val konotop = Konotop
         .Builder()
-        .client(httpClient)
+        .baseUrl("https://dummyjson.com/")
         .build()
 
     @Test
--- runtime/src/commonMain/kotlin/konotop/Konotop.kt
+++ runtime/src/commonMain/kotlin/konotop/Konotop.kt
@@ -1,8 +1,14 @@
 package konotop
 
 import io.ktor.client.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.utils.io.core.*
+import kotlinx.serialization.json.Json
 import kotlin.reflect.KClass
+
 
 class Konotop private constructor(
     private val client: HttpClient
@@ -15,12 +21,24 @@ class Konotop private constructor(
         val factory = service.lookupFactory()
             ?: throw RuntimeException("Unknown service: ${service.qualifiedName}")
 
-        @Suppress("UNCHECKED_CAST")
-        return factory(client) as T
+        return factory(client)
     }
 
     class Builder {
         private var client: HttpClient? = null
+        private var baseUrl: Url? = null
+
+        fun baseUrl(baseUrl: String) = apply {
+            baseUrl(Url(baseUrl))
+        }
+
+        fun baseUrl(baseUrl: Url) = apply {
+            val pathSegments: List<String> = baseUrl.pathSegments
+            if ("" != pathSegments[pathSegments.size - 1]) {
+                throw IllegalArgumentException("baseUrl must end in /: $baseUrl")
+            }
+            this.baseUrl = baseUrl
+        }
 
         fun client(client: HttpClient): Builder {
             this.client = client
@@ -28,7 +46,31 @@ class Konotop private constructor(
         }
 
         fun build(): Konotop {
-            return Konotop(client!!)
+            val client = client ?: defaultClient()
+            return Konotop(client.applyConfig())
+        }
+
+        private fun HttpClient.applyConfig() = config {
+            val baseUrl = baseUrl
+            if (baseUrl != null) {
+                applyBaseUrl(baseUrl)
+            }
+        }
+
+        private fun HttpClientConfig<*>.applyBaseUrl(baseUrl: Url) {
+            defaultRequest {
+                url(baseUrl.toString())
+            }
+        }
+    }
+
+    companion object {
+        fun defaultClient() = HttpClient {
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                })
+            }
         }
     }
 }
